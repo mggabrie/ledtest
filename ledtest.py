46cae3b42450070e2c7e5c0d489d30f80eb105e3
mggabrie <40970426+mggabrie@users.noreply.github.com>
2018-07-12 13:32:35 -0400

    Update ledtest.py

--- ledtest.py
+++ ledtest.py
@@ -1,11 +1,19 @@
+from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
-from luma.led_matrix.device import max7219
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
-serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial, cascaded=4, block_orientation=-90)
 
-font = ImageFont.truetype("examples/pixelmix.ttf", 8)
+def message(n, block_orientation, rotate):
+    # create matrix device
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation, rotate=rotate or 0)
+    print("Created device")
 
-with canvas(device) as draw:
-    draw.rectangle(device.bounding_box, outline="white", fill="black")
+    # start demo
+    msg = "MAX7219 LED Matrix Demo"
+    print(msg)
+    show_message(device, msg, fill="white", font=proportional(CP437_FONT))
+    time.sleep(1)
