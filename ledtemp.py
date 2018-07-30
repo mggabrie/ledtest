90453daea4d3c691f41c827838775eba2e350c6d
mggabrie <40970426+mggabrie@users.noreply.github.com>
2018-07-30 01:22:53 -0400

    Create ledtemp.py

---
+++ ledtemp.py
@@ -0,0 +1,34 @@
+import time
+import bs4
+import urllib.request
+
+from time import localtime, strftime
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+
+def led():
+	msg = strftime("%I:%M%P", localtime())
+	with canvas(device) as draw:
+		text(draw, (0, 0), msg, fill="white", font=proportional(LCD_FONT))
+def temp():
+	url = "https://weather.com/weather/today/l/10461:4:US"
+	html = urllib.request.urlopen(url)
+	soup_html = bs.BeautifulSoup(html, "html.parser")
+	temp_html = soup_html.find("div", attrs={"class": "today_nowcard-temp"})
+	msg = temp_html.text.strip()
+	with canvas(device) as draw:
+		text(draw, (0, 0), msg, fill="white", font=proportional(LCD_FONT))
+
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
+device.contrast(50)
+
+while True:
+	for i in range(10):
+		led()
+		time.sleep(1)
+	temp()
+	time.sleep(10)
