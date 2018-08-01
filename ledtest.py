fc490d8fea79522618526383cb2bc37da136c110
mggabrie <40970426+mggabrie@users.noreply.github.com>
2018-08-01 01:01:32 -0400

    Update ledtest.py

--- ledtest.py
+++ ledtest.py
@@ -20,31 +20,18 @@ def temp():
 	temp_html = soup_html.find("div", attrs={"class": "today_nowcard-temp"})
 	msg = temp_html.text.strip()
 	with canvas(device) as draw:
-		text(draw, (0, 0), (msg[0:2]+chr(247)+"F"), fill="white", font=proportional(TINY_FONT))
-def fadein():
-	for intensity in range(4):
-		device.contrast(intensity*10)
-		time.sleep(.1)	
-def fadeout():
-	for intensity in reversed(range(4)):
-		device.contrast(intensity*10)
-		time.sleep(.1)
+		text(draw, (0, 0), (msg[0:2]+"F"), fill="white", font=proportional(TINY_FONT))
 		
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
-device.contrast(0)
+device.contrast(10)
 
 while True:
-	led()
-	fadein()
 	for i in range(10):
 		led()
 		time.sleep(1)
-	fadeout()
 	try:
 		temp()
 	except:
 		continue
-	fadein()
 	time.sleep(10)
-	fadeout()
