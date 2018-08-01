0f1f2b66b44089a3854605ef015c5848463a83fa
mggabrie <40970426+mggabrie@users.noreply.github.com>
2018-08-01 00:37:51 -0400

    Update ledtext.py

--- ledtext.py
+++ ledtext.py
@@ -3,10 +3,11 @@ from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
 from luma.core.legacy import text, show_message
-from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+from luma.core.legacy.font import proportional, TINY_FONT
 
 def led():
-	msg = input("Enter your message: ")
+	msg = input("Enter intensity 0-255: ")
+	device.contrast(int(msg))
 	with canvas(device) as draw:
 		for i in range(4):
 			text(draw, (0, 0), msg, fill="white", font=proportional(TINY_FONT))
@@ -14,8 +15,6 @@ def led():
 
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
-device.contrast(50)
-font = [CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT]
 
 while True:
 	led()
