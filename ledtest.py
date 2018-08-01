d80f454b6bd5f3e42cc36a0e17740a4a18e79870
mggabrie <40970426+mggabrie@users.noreply.github.com>
2018-08-01 00:29:52 -0400

    Update ledtest.py

--- ledtest.py
+++ ledtest.py
@@ -7,12 +7,12 @@ from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
 from luma.core.legacy import text, show_message
-from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+from luma.core.legacy.font import proportional, TINY_FONT
 
 def led():
 	msg = strftime("%I:%M", localtime())
 	with canvas(device) as draw:
-		text(draw, (0, 0), msg, fill="white", font=proportional(LCD_FONT))
+		text(draw, (0, 0), msg, fill="white", font=proportional(TINY_FONT))
 def temp():
 	url = "https://weather.com/weather/today/l/10461:4:US"
 	html = urllib.request.urlopen(url)
@@ -20,18 +20,31 @@ def temp():
 	temp_html = soup_html.find("div", attrs={"class": "today_nowcard-temp"})
 	msg = temp_html.text.strip()
 	with canvas(device) as draw:
-		text(draw, (0, 0), (msg[0:2]+chr(247)+"F"), fill="white", font=proportional(LCD_FONT))
-
+		text(draw, (0, 0), (msg[0:2]+chr(247)+"F"), fill="white", font=proportional(TINY_FONT))
+def fadein():
+	for intensity in range(4):
+		device.contrast(intensity*10)
+		time.sleep(.1)	
+def fadeout():
+	for intensity in reversed(range(4)):
+		device.contrast(intensity*10)
+		time.sleep(.1)
+		
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
-device.contrast(50)
+device.contrast(0)
 
 while True:
+	led()
+	fadein()
 	for i in range(10):
 		led()
 		time.sleep(1)
+	fadeout()
 	try:
 		temp()
 	except:
 		continue
+	fadein()
 	time.sleep(10)
+	fadeout()
